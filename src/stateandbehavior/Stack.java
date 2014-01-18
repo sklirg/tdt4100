b7147238e68a2124e4a5f30374a9601a3bdf3497
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-01-18 20:57:29 +0100

    Finished Stack

--- src/stateandbehavior/Stack.java
+++ src/stateandbehavior/Stack.java
@@ -1,7 +1,65 @@
 package stateandbehavior;
+import java.util.ArrayList;
 
-/**
- * Created by haakon on 18/01/14.
- */
 public class Stack {
+    ArrayList StringList;
+
+    {
+        StringList = new ArrayList<String>();
+    }
+
+    public void push(String str)
+    {
+        this.StringList.add(str);
+    }
+
+    public String pop()
+    {
+        try {
+            String r;
+            r = (String) this.StringList.get(this.StringList.size() - 1);
+            this.StringList.remove(this.StringList.size() - 1);
+            return r;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    public String peek(int i)
+    {
+        try {
+            return (String) this.StringList.get(this.StringList.size()-1-i);
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    public int getSize()
+    {
+        return this.StringList.size();
+    }
+
+    public static void main (String[] Args)
+    {
+        Stack s = new Stack();
+        RandomStringGenerator rString = new RandomStringGenerator();
+
+        s.push(rString.getRandomString());
+        System.out.println(s.StringList);
+        s.push(rString.getRandomString());
+        System.out.println(s.StringList);
+        s.push(rString.getRandomString());
+        System.out.println(s.StringList);
+
+        System.out.println(s.pop());
+        System.out.println(s.StringList);
+        System.out.println(s.pop());
+        System.out.println(s.StringList);
+        System.out.println(s.pop());
+        System.out.println(s.StringList);
+    }
 }
