4c02f721924bf671ac8f5dcc5cef76d9116e37e1
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-13 19:02:01 +0100

    initial getters and setters for partner

--- src/objectstructures/Partner.java
+++ src/objectstructures/Partner.java
@@ -4,4 +4,40 @@ package objectstructures;
  * Created by sklirg on 2/13/14.
  */
 public class Partner {
+    String name;
+    Partner partner;
+
+    public Partner(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Partner getPartner() {
+        return partner;
+    }
+
+    public void setPartner(Partner partner) {
+        if (this.partner != null)
+            throw new IllegalArgumentException("This person already has a partner.");
+        else
+            this.partner = partner;
+    }
+
+    public void setPartner() {
+        if (this.partner.getPartner() != null)
+            this.partner.setPartner();
+        this.partner = null;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
