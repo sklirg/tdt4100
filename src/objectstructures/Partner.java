8665afa2f6010bb3328fabb92189c7eb1fe97a80
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-13 21:43:31 +0100

    adding resetting of partners if null as arg

--- src/objectstructures/Partner.java
+++ src/objectstructures/Partner.java
@@ -5,7 +5,7 @@ package objectstructures;
  */
 public class Partner {
     String name;
-    Partner partner;
+    Partner partner = null;
 
     public Partner(String name) {
         this.name = name;
@@ -20,16 +20,73 @@ public class Partner {
     }
 
     public void setPartner(Partner partner) {
-        if (this.partner != null)
-            throw new IllegalArgumentException("This person already has a partner.");
-        else
-            this.partner = partner;
-    }
+        if (partner == null) {
+            // Do resetting stuff here
+            boolean hasPartner;
+            if (this.getPartner() == null) {
+                hasPartner = false;
+            }
+            else {
+                hasPartner = true;
+            }
 
-    public void setPartner() {
-        if (this.partner.getPartner() != null)
-            this.partner.setPartner();
-        this.partner = null;
+            if (hasPartner) {
+                Partner other = this.getPartner();
+                this.partner = null;
+                other.setPartner(null);
+            }
+            else
+                this.partner = null;
+        }
+        else {
+            // Set new partners
+            if (this.partner != null) {
+                //throw new IllegalArgumentException("This person already has a partner. ["+this+"]");
+                // If currently has partner
+                if (this.partner.getPartner() != null && false)
+                {
+                    /* @ToDo
+                     * Swingers;
+                      * If setting new partner while having partner, reset all and set up new relationship.
+                     */
+                    Partner oldPartner = this.partner.getPartner();
+                    if (oldPartner.getPartner() != null) {
+                        System.out.println("Old partner has partner");
+                        oldPartner.setPartner(null);
+                    }
+                    this.partner = null;
+
+                    Partner newPartner = partner;
+                    boolean newPartnerHasPartner;
+
+                    if (newPartner.getPartner() != null) {
+                        System.out.println("New partner has partner");
+                        newPartner.setPartner(null);
+                    }
+                    System.out.println("Setting new partners");
+                    /*newPartner.setPartner(this);
+                    this.partner = newPartner;*/
+                }
+            }
+            else {
+                // If no current partner
+                if (false) {
+                    Partner oldPartner = this.getPartner();
+                    Partner newPartner = partner;
+                    this.partner = null;
+                    oldPartner.setPartner(null);
+                    if (newPartner.getPartner() != null) {
+                        newPartner.setPartner(null);
+                    }
+
+                    this.setPartner(partner);
+                }
+                else {
+                    this.partner = partner;
+                    partner.setPartner(this);
+                }
+            }
+        }
     }
 
     @Override
@@ -38,6 +95,18 @@ public class Partner {
     }
 
     public static void main(String[] args) {
-
+        Partner far = new Partner("Far");
+        Partner mor = new Partner("Mor");
+        System.out.println("======initial");
+        System.out.println("Fars partner: " + far.getPartner());
+        System.out.println("Mors partner: " + mor.getPartner());
+        System.out.println("======Setting partners");
+        far.setPartner(mor);
+        System.out.println("Fars partner: " + far.getPartner());
+        System.out.println("Mors partner: " + mor.getPartner());
+        System.out.println("======Removing partners");
+        far.setPartner(null);
+        System.out.println("Fars partner: " + far.getPartner());
+        System.out.println("Mors partner: " + mor.getPartner());
     }
 }
