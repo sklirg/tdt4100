ccae38454fdc0944a864791f4170546b0d691982
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-14 18:30:06 +0100

    required methods for deck and hand

--- src/objectstructures/CardDeck.java
+++ src/objectstructures/CardDeck.java
@@ -1,7 +1,35 @@
 package objectstructures;
-
+import java.util.ArrayList;
 /**
  * Created by sklirg on 2/14/14.
  */
 public class CardDeck {
+    ArrayList<Card> cards = new ArrayList<Card>();
+
+    public CardDeck(int n) {
+        int total = n*4;
+
+    }
+
+    public int getCardCount() {
+        return cards.size();
+    }
+
+    public Card getCard(int n) {
+        try {
+            return cards.get(n);
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("This card does not exist");
+        }
+    }
+
+    public void deal(CardHand hand, int n) {
+
+    }
+
+    public void shufflePerfectly() {
+
+    }
+
 }
--- src/objectstructures/CardHand.java
+++ src/objectstructures/CardHand.java
@@ -1,7 +1,29 @@
 package objectstructures;
-
+import java.util.ArrayList;
 /**
  * Created by sklirg on 2/14/14.
  */
 public class CardHand {
+    ArrayList<Card> cards = new ArrayList<Card>();
+
+    public int getCardCount() {
+        return 0;
+    }
+
+    public Card getCard(int n) {
+        try {
+            return cards.get(n);
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("This card does not exist");
+        }
+    }
+
+    public void addCard(Card card) {
+
+    }
+
+    public Card play(int n) {
+        return cards.get(n);
+    }
 }
