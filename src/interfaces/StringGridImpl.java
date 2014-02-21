fe0c011855186d77f38f08c97cab653a7178a898
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-21 11:51:45 +0100

    added implemented methods to StringGridImpl

--- src/interfaces/StringGridImpl.java
+++ src/interfaces/StringGridImpl.java
@@ -1,7 +1,31 @@
 package interfaces;
 
+import java.util.ArrayList;
+
 /**
  * Created by sklirg on 2/21/14.
  */
-public class StringGridImpl {
+public class StringGridImpl implements IStringGrid {
+    ArrayList<ArrayList<String>> stringGrid = new ArrayList<ArrayList<String>>();
+
+    /*public StringGridImpl() {
+
+    }
+    */
+
+    public int getRowCount() {
+        return stringGrid.size();
+    }
+
+    public int getColumnCount() {
+        return 1; // return something useful
+    }
+
+    public void setElement(int row, int col) {
+        // set
+    }
+
+    public String getElement(int row, int col) {
+        return "";
+    }
 }
