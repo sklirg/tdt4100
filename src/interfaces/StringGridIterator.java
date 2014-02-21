7cb987799554637a950e7ab2d8ca38e3aa326819
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-21 12:26:24 +0100

    initial commit for StringGridIterator

--- src/interfaces/StringGridIterator.java
+++ src/interfaces/StringGridIterator.java
@@ -1,7 +1,22 @@
 package interfaces;
 
+import java.util.Iterator;
+
 /**
  * Created by sklirg on 2/21/14.
  */
-public class StringGridIterator {
+public class StringGridIterator implements Iterator<String> {
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public String next() {
+        StringGridImpl r = new StringGridImpl();
+        return r.getElement(0,0);
+    }
+
+    public void remove() {
+        // Do Stuff
+    }
 }
