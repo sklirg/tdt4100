17557a2604b7736d3f9bcaac7d34a86b6f658177
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-21 12:17:03 +0100

    added exceptions for methods, added todo

--- src/interfaces/StringGridImpl.java
+++ src/interfaces/StringGridImpl.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 public class StringGridImpl implements IStringGrid {
     ArrayList<ArrayList<String>> stringGrid = new ArrayList<ArrayList<String>>();
 
+    /* @ToDo
+     * Constructor
+     */
     /*public StringGridImpl() {
 
     }
@@ -18,14 +21,29 @@ public class StringGridImpl implements IStringGrid {
     }
 
     public int getColumnCount() {
-        return 1; // return something useful
+        try {
+            return stringGrid.get(0).size();
+        }
+        catch (Exception e) {
+            return -1;
+        }
     }
 
-    public void setElement(int row, int col) {
-        // set
+    public void setElement(int row, int col, String str) {
+        try {
+            stringGrid.get(row).add(col, str);
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("Something went wrong!\n" + e);
+        }
     }
 
     public String getElement(int row, int col) {
-        return "";
+        try {
+            return stringGrid.get(row).get(col);
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("HTTP 404 . There is nothing here");
+        }
     }
 }
