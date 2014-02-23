8165c9c0b88a2b0f8d84b5ae93e88b8be8c910a6
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-23 15:08:24 +0100

    initial for Person1

--- src/interfaces/named/Person1.java
+++ src/interfaces/named/Person1.java
@@ -2,6 +2,34 @@ package interfaces.named;
 
 /**
  * Created by sklirg on 2/23/14.
+ *
+ * Class for given and family name
+ *
  */
-public class Person1 {
+public class Person1 implements INamed{
+    String givenName, familyName, fullName;
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
