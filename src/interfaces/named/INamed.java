a7fb1b0d7930f21ccac36c5e18139d8d274174b2
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-23 15:07:26 +0100

    interface for named

--- src/interfaces/named/INamed.java
+++ src/interfaces/named/INamed.java
@@ -4,4 +4,12 @@ package interfaces.named;
  * Created by sklirg on 2/23/14.
  */
 public interface INamed {
+    void setGivenName(String givenName);
+    String getGivenName();
+
+    void setFamilyName(String familyName);
+    String getFamilyName();
+
+    void setFullName(String fullName);
+    String getFullName();
 }
