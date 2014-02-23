5af8e6e361b3d85d9dde06b58ee42a5817eeb2f0
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-23 15:57:02 +0100

    initial for comparator

--- src/interfaces/named/NamedComparator.java
+++ src/interfaces/named/NamedComparator.java
@@ -1,7 +1,13 @@
 package interfaces.named;
 
+import java.util.Comparator;
+
 /**
  * Created by sklirg on 2/23/14.
+ *
+ * Comparator for Person1 and Person2 classes.
+ *
  */
-public class NamedComparator {
+public class NamedComparator implements Comparator {
+
 }
