63147f6c4cc4f4d15ce3025c6679a2085b7bbc85
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-02-23 16:06:12 +0100

    implementing comparator for INamed

--- src/interfaces/named/NamedComparator.java
+++ src/interfaces/named/NamedComparator.java
@@ -8,6 +8,22 @@ import java.util.Comparator;
  * Comparator for Person1 and Person2 classes.
  *
  */
-public class NamedComparator implements Comparator {
+public class NamedComparator implements Comparator<INamed> {
+
+/*
+    public NamedComparator() {
+
+    }*/
+
+    public int compare(INamed name1, INamed name2) {
+        int diffFamilyName = name1.getFamilyName().compareTo(name2.getFamilyName());
+        int diffGivenName = name1.getGivenName().compareTo(name2.getGivenName());
+        if (diffFamilyName != 0)
+            return diffFamilyName;
+        else if (diffGivenName != 0)
+            return name1.getGivenName().compareTo(name2.getGivenName());
+        else
+            return 0;
+    }
 
 }
