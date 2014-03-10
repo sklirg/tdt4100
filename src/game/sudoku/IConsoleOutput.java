4e7e7050598d8985fc394a2dfa7bed70ec8f3f76
Sklirg <hlsolbjorg@gmail.com>
2014-03-10 17:40:11 +0200

    added gamedriver and new interface

---
+++ src/game/sudoku/IConsoleOutput.java
@@ -0,0 +1,10 @@
+package game.sudoku;
+
+/**
+ * Created by Håkon on 10.03.14.
+ */
+public interface IConsoleOutput {
+    public void message(String message);
+
+    public void error(String message);
+}
