9266f2a2b45af3dc21b6cd5dbced06ec521d045e
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-03-19 13:21:44 +0100

    initial traincar methods

--- src/inheritance/TrainCar.java
+++ src/inheritance/TrainCar.java
@@ -1,5 +1,21 @@
 package inheritance;
 
 public class TrainCar {
+    private int emptyWeight, deadWeight;
 
+    TrainCar(int weight){
+        this.emptyWeight = weight;
+    }
+
+    public int getTotalWeight() {
+        return this.emptyWeight + this.deadWeight;
+    }
+
+    public int getDeadWeight() {
+        return deadWeight;
+    }
+
+    public void setDeadWeight(int deadWeight) {
+        this.deadWeight = deadWeight;
+    }
 }
