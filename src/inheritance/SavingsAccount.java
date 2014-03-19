3c2a92ea6db2dffff2ded837402eb2795fb34593
Sklirg <hlsolbjorg@gmail.com>
2014-03-19 10:37:32 +0100

    added methods and todos

--- src/inheritance/SavingsAccount.java
+++ src/inheritance/SavingsAccount.java
@@ -1,5 +1,30 @@
 package inheritance;
 
-public class SavingsAccount {
+public class SavingsAccount implements IAccount{
+    private double balance, rentefot;
 
+    public SavingsAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+     // @Todo: illegal amount throws new IllegalArgException
+    }
+
+    public void withdraw(double amount) {
+        /*
+         * @Todo
+         * Illegal amount throws new IllArgExc
+         * Not enough funds throws new IllStateExc
+         */
+    }
+
+    public double getBalance() {
+        // @ Todo return balance
+        return -1;
+    }
+
+    public void endYearUpdate() {
+        // @Todo apply rentefot
+    }
 }
