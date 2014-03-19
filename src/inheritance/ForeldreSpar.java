6ba3a55be6611c4311ca6afa48610b26eb61b225
Sklirg <hlsolbjorg@gmail.com>
2014-03-19 11:45:27 +0100

    ctor

--- src/inheritance/ForeldreSpar.java
+++ src/inheritance/ForeldreSpar.java
@@ -1,4 +1,10 @@
 package inheritance;
 
-public class ForeldreSpar {
+public class ForeldreSpar extends SavingsAccount{
+    private int limit, withdrawalsThisYear;
+
+    public ForeldreSpar(double rentefot, int allowedNumOfWithdrawal) {
+        super(rentefot);
+        this.limit = allowedNumOfWithdrawal;
+    }
 }
