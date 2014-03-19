4ad0106196fba77776b9ab45b1c0309b104c44fb
Sklirg <hlsolbjorg@gmail.com>
2014-03-19 10:30:35 +0100

    added interface methods

--- src/inheritance/IAccount.java
+++ src/inheritance/IAccount.java
@@ -4,4 +4,7 @@ package inheritance;
  * Created by Håkon on 19.03.14.
  */
 public interface IAccount {
+    public void deposit(double amount);
+    public void withdraw(double amount);
+    public double getBalance();
 }
