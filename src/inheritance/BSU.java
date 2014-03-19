a635fc87b8f96f56d2fffd42b8818eb0f79280b5
Sklirg <hlsolbjorg@gmail.com>
2014-03-19 11:09:50 +0100

    initial BSU

--- src/inheritance/BSU.java
+++ src/inheritance/BSU.java
@@ -1,4 +1,18 @@
 package inheritance;
 
-public class BSU {
+public class BSU extends SavingsAccount{
+    private double limit, depositsLastYear;
+
+    public BSU(double rentefot, double limit) {
+        super(rentefot);
+        this.limit = limit;
+    }
+
+    public void deposit(double amount) {
+
+    }
+
+    public double getTaxDeduction() {
+
+    }
 }
