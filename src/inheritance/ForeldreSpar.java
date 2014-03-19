a34ec11736e81fa395d577612524773906e66a84
Sklirg <hlsolbjorg@gmail.com>
2014-03-19 11:48:04 +0100

    initial layout

--- src/inheritance/ForeldreSpar.java
+++ src/inheritance/ForeldreSpar.java
@@ -1,10 +1,18 @@
 package inheritance;
 
 public class ForeldreSpar extends SavingsAccount{
-    private int limit, withdrawalsThisYear;
+    private int limit, remainingWithdrawals;
 
     public ForeldreSpar(double rentefot, int allowedNumOfWithdrawal) {
         super(rentefot);
         this.limit = allowedNumOfWithdrawal;
+    }
+
+    public void withdraw() {
+        // @Todo: validate according to limit
+    }
+
+    public int getRemainingWithdrawals() {
+        return this.remainingWithdrawals;
     }
 }
