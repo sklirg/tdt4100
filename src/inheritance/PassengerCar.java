b30cd712cac0bc1be0ea492f7cdc4fb2c81a10d2
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-03-19 13:30:01 +0100

    initial passengercar methods

--- src/inheritance/PassengerCar.java
+++ src/inheritance/PassengerCar.java
@@ -1,4 +1,18 @@
 package inheritance;
 
-public class PassengerCar {
+public class PassengerCar extends TrainCar {
+    private int passengerCount;
+
+    public PassengerCar(int emptyWeight, int passengerCount) {
+        super(emptyWeight);
+        this.passengerCount = passengerCount;
+    }
+
+    public int getPassengerCount() {
+        return passengerCount;
+    }
+
+    public void setPassengerCount(int passengerCount) {
+        this.passengerCount = passengerCount;
+    }
 }
