7d8b486d0c6b32189fa5bc5efef389e14d0bf323
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-03-19 13:25:49 +0100

    initial cargocar

---
+++ src/inheritance/CargoCar.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class CargoCar extends TrainCar {
+    private int cargoWeight;
+
+    public CargoCar(int emptyWeight, int cargoWeight) {
+        super(emptyWeight);
+        this.cargoWeight = cargoWeight;
+    }
+
+    public int getCargoWeight() {
+        return cargoWeight;
+    }
+
+    public void setCargoWeight(int cargoWeight) {
+        this.cargoWeight = cargoWeight;
+    }
+}
