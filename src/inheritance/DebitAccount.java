6b83b923a0ee0e990dc649b0a92de73247bd9c01
Sklirg <hlsolbjorg@gmail.com>
2014-03-21 18:32:02 +0100

    lalalalala debitwtf

--- src/inheritance/DebitAccount.java
+++ src/inheritance/DebitAccount.java
@@ -1,4 +1,12 @@
 package inheritance;
 
 public class DebitAccount extends AbstractAccount{
+
+    public DebitAccount() {
+        super();
+    }
+
+    public void internalWithdraw(double amount) {
+        super.withdraw(amount);
+    }
 }
