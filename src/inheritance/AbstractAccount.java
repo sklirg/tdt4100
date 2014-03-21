541e3f769175fa15c2a40172602f137c6812ab45
Sklirg <hlsolbjorg@gmail.com>
2014-03-21 18:19:49 +0100

    initial methods

--- src/inheritance/AbstractAccount.java
+++ src/inheritance/AbstractAccount.java
@@ -1,4 +1,19 @@
 package inheritance;
 
-public class AbstractAccount {
+public abstract class AbstractAccount {
+    private double balance;
+
+    public void deposit(double amount) {
+
+    }
+
+    public void withdraw(double amount) {
+
+    }
+
+    abstract void internalWithdraw(double amount);
+
+    public double getBalance() {
+
+    }
 }
