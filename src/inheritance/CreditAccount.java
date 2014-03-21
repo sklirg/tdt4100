0e2f9dc68931ac56ffb508864aa2b5bda9f29776
Sklirg <hlsolbjorg@gmail.com>
2014-03-21 19:17:51 +0100

    initial methods

--- src/inheritance/CreditAccount.java
+++ src/inheritance/CreditAccount.java
@@ -1,4 +1,17 @@
 package inheritance;
 
 public class CreditAccount extends AbstractAccount {
+    private double creditLine;
+
+    public CreditAccount(double limit) {
+
+    }
+
+    public double getCreditLine() {
+        return creditLine;
+    }
+
+    public void setCreditLine(double creditLine) {
+        this.creditLine = creditLine;
+    }
 }
