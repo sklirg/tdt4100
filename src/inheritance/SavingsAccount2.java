748f26df40592347fae81ecd9a86f687b83aa56e
Sklirg <hlsolbjorg@gmail.com>
2014-03-21 22:32:26 +0100

    initial methods

--- src/inheritance/SavingsAccount2.java
+++ src/inheritance/SavingsAccount2.java
@@ -1,5 +1,16 @@
 package inheritance;
 
-public class SavingsAccount2 {
+public class SavingsAccount2 extends AbstractAccount {
+    private int withdrawalLimit, currentWithdrawals;
+    private double fee;
 
+    public SavingsAccount2(int limit, double fee) {
+        super();
+        this.withdrawalLimit = limit;
+        this.fee = fee;
+    }
+
+    public void internalWithdraw(double amount) {
+
+    }
 }
