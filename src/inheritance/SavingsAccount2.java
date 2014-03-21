8e8ea3aa4f3b86f41695c48ec80cc690b5df0b91
Sklirg <hlsolbjorg@gmail.com>
2014-03-21 22:39:58 +0100

    some stuff for withdraw method

--- src/inheritance/SavingsAccount2.java
+++ src/inheritance/SavingsAccount2.java
@@ -8,9 +8,13 @@ public class SavingsAccount2 extends AbstractAccount {
         super();
         this.withdrawalLimit = limit;
         this.fee = fee;
+        this.currentWithdrawals = 0;
     }
 
     public void internalWithdraw(double amount) {
-
+        if (currentWithdrawals >= withdrawalLimit)
+            throw new IllegalStateException("Not enough withdrawals");
+        else if (amount > super.getBalance())
+            throw new IllegalStateException("Not enough funds");
     }
 }
