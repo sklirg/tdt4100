87ff005290438d818ab6967fbc63f4f2c3d7f2b4
Sklirg <hlsolbjorg@gmail.com>
2014-03-21 18:41:02 +0100

    initial internalWithdraw

--- src/inheritance/DebitAccount.java
+++ src/inheritance/DebitAccount.java
@@ -7,5 +7,7 @@ public class DebitAccount extends AbstractAccount {
     }
 
     public void internalWithdraw(double amount) {
+        if (super.getBalance()<amount)
+            throw new IllegalStateException("Not enough funds");
     }
 }
