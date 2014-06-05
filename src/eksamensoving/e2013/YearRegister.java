0d3cf624011b2b8392c91bff7c3cfcf761e3fa2d
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-06-05 17:47:11 +0200

    3c1

--- src/eksamensoving/e2013/YearRegister.java
+++ src/eksamensoving/e2013/YearRegister.java
@@ -1,13 +1,35 @@
-package tdt4100.eksamensoving.e2013;
+package eksamensoving.e2013;
+
+import java.util.*;
 
 public class YearRegister {
+    private Collection<TaxFirm> firms;
+    private Collection<TaxPerson> persons;
+
     public final int year;
 
     public YearRegister(int year) {
         this.year = year;
+        this.firms = new HashSet<TaxFirm>();
+        this.persons = new HashSet<TaxPerson>();
     }
 
     public String toString() {
-        return String.format("YearRegister %s: %s TaxEntities. Max tax is %s, Min tax is %s, Average tax is %s", year, getNumEntities(), getMaxTax(), getMinTax(), getAvgTax()); // fix
+        return String.format("YearRegister %s: %s TaxEntities. Max tax is %s, Min tax is %s, Average tax is %s", year, numEntities, maxTax, minTax, avgTax); // fix
+    }
+
+    public boolean containsTaxEntity(String id) {
+        if (id.length() == TaxPerson.LENGTH) {
+            for (TaxPerson tp : persons) {
+                if (tp.getId().equals(id)) return true;
+            }
+            return false;
+        }
+        else if (id.length() == TaxFirm.LENGTH) {
+            for (TaxFirm tf : firms) {
+                if (tf.getId().equals(id)) return true;
+            }
+            return false;
+        }
     }
 }
