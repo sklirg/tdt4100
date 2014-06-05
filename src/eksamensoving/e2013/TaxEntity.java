54a30a45745475e1ccc4f6b6debfc105067bb3eb
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-06-05 13:13:55 +0200

    oppgave 1b

--- src/eksamensoving/e2013/TaxEntity.java
+++ src/eksamensoving/e2013/TaxEntity.java
@@ -4,8 +4,6 @@ public abstract class TaxEntity {
 
     /*
      * Oppgave 1
-     *
-     *
      */
     private final String id;
     private String navn;
@@ -15,4 +13,40 @@ public abstract class TaxEntity {
         this.id = id;
         this.navn = navn;
     }
+
+    /*
+     * Oppgave 2
+     */
+    private boolean checkPositive(long num) {
+        return num >= 0;
+    }
+
+    // overloading for int
+    private boolean checkPositive(int num) {
+        return num >= 0;
+    }
+
+    private boolean checkPercent(double percent) {
+        return (percent >= 0 && percent <= 100);
+    }
+
+    private boolean checkName(String name) {
+        String alphabet = "abcdefghijklmnopqrstuvwxyzæøå";
+
+        // First, check length of name >= 3.
+        if (name.length() < 3)
+            return false;
+        else {
+            int numNorskeChars = 0;
+            for (char c : name.toCharArray()) {
+                if (numNorskeChars >= 3)
+                    return true;
+                if (Character.isLetter(c))
+                    numNorskeChars++;
+            }
+        }
+        return false;
+    }
+
+    protected abstract boolean checkId(String id);
 }
