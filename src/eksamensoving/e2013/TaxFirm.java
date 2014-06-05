969b1777d9ef9761927bca06193f4569627bc0d3
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-06-05 14:46:02 +0200

    Oppgave 2

--- src/eksamensoving/e2013/TaxFirm.java
+++ src/eksamensoving/e2013/TaxFirm.java
@@ -1,6 +1,20 @@
 package eksamensoving.e2013;
 
 public class TaxFirm extends TaxEntity {
-    private static final int
+    private static final int length = 9;
 
+    protected TaxFirm(String id, String navn, double skatteprosent) {
+        super(id,navn,skatteprosent);
+    }
+
+    @Override
+    protected boolean checkId(String id) {
+        if (id.length() != length) return false;
+        else {
+            for (char c : id.toCharArray()) {
+                if (! Character.isDigit(c)) return false;
+            }
+            return true;
+        }
+    }
 }
