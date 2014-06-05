b78048645ab5428b10df8bbcb1b731394368ae32
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-06-05 14:35:04 +0200

    plz

--- src/eksamensoving/e2013/TaxFirm.java
+++ src/eksamensoving/e2013/TaxFirm.java
@@ -1,5 +1,6 @@
 package eksamensoving.e2013;
 
 public class TaxFirm extends TaxEntity {
+    private static final int
 
 }
--- src/eksamensoving/e2013/TaxPerson.java
+++ src/eksamensoving/e2013/TaxPerson.java
@@ -1,11 +1,18 @@
 package eksamensoving.e2013;
 
 public class TaxPerson extends TaxEntity {
+    private static final int length = 11;
+
     public TaxPerson(String id, String navn, double skatteprosent) {
         super(id, navn, skatteprosent);
     }
 
     protected boolean checkId(String id) {
-        return id.length() == 11;
+        if (id.length() != length) return false;
+        else {
+            for (char c : id.toCharArray())
+                if (! Character.isDigit(c)) return false;
+            return true;
+        }
     }
 }
