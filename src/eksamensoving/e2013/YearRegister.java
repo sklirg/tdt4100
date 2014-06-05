dd68618a3199988f03b0fa0f076843111a769a5a
Håkon Solbjørg <hlsolbjorg@gmail.com>
2014-06-05 17:53:39 +0200

    task wasn't so specific

--- src/eksamensoving/e2013/YearRegister.java
+++ src/eksamensoving/e2013/YearRegister.java
@@ -3,33 +3,23 @@ package eksamensoving.e2013;
 import java.util.*;
 
 public class YearRegister {
-    private Collection<TaxFirm> firms;
-    private Collection<TaxPerson> persons;
+    private Collection<TaxEntity> entities;
 
     public final int year;
 
     public YearRegister(int year) {
         this.year = year;
-        this.firms = new HashSet<TaxFirm>();
-        this.persons = new HashSet<TaxPerson>();
+        this.entities= new HashSet<TaxEntity>();
     }
 
     public String toString() {
-        return String.format("YearRegister %s: %s TaxEntities. Max tax is %s, Min tax is %s, Average tax is %s", year, numEntities, maxTax, minTax, avgTax); // fix
+        return String.format("YearRegister %s: %s TaxEntities. Max tax is %s, Min tax is %s, Average tax is %s", year, entities.size(), maxTax, minTax, avgTax); // fix
     }
 
     public boolean containsTaxEntity(String id) {
-        if (id.length() == TaxPerson.LENGTH) {
-            for (TaxPerson tp : persons) {
-                if (tp.getId().equals(id)) return true;
-            }
-            return false;
+        for (TaxEntity te : this.entities) {
+            if (te.getId().equals(id)) return true;
         }
-        else if (id.length() == TaxFirm.LENGTH) {
-            for (TaxFirm tf : firms) {
-                if (tf.getId().equals(id)) return true;
-            }
-            return false;
-        }
+        return false;
     }
 }
